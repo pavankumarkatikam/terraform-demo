26fe9888153fa129cd0fc40baba24b7edeaa2650
pavankumarkatikam <42901841+pavankumarkatikam@users.noreply.github.com>
2023-03-21 00:33:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,19 +11,7 @@ terraform {
 resource "aws_ecr_repository" "app_ecr_repo" {
   name = "app-repo"
 
-  provisioner "local-exec" {
-    command = "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${aws_ecr_repository.app_ecr_repo.repository_url}"
-  }
-
-  provisioner "local-exec" {
-    command = "docker build -t myproject ../"
-  }
-  provisioner "local-exec" {
-    command = "docker tag myproject:latest ${aws_ecr_repository.app_ecr_repo.repository_url}/myproject:latest"
-  }
-  provisioner "local-exec" {
-    command = "docker push ${aws_ecr_repository.app_ecr_repo.repository_url}/myproject:latest"
-  }
+  command = "export REPOSITORY_URL=${aws_ecr_repository.app_ecr_repo.repository_url}"
   
 }
 
