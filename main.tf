9b755e53c722aa77b1c542a44e73c642901af595
pavankumarkatikam <42901841+pavankumarkatikam@users.noreply.github.com>
2023-03-20 23:50:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.45.0"
+    }
+  }
+}
+
+
+resource "aws_ecr_repository" "app_ecr_repo" {
+  name = "app-repo"
+
+  provisioner "local-exec" {
+    command = "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${aws_ecr_repository.app_ecr_repo.repository_url}"
+  }
+
+  provisioner "local-exec" {
+    command = "docker build -t myproject ."
+  }
+  provisioner "local-exec" {
+    command = "docker tag myproject:latest ${aws_ecr_repository.app_ecr_repo.repository_url}/myproject:latest"
+  }
+  provisioner "local-exec" {
+    command = "docker push ${aws_ecr_repository.app_ecr_repo.repository_url}/myproject:latest"
+  }
+  
+}
+
+
+output "name" {
+    value = aws_ecr_repository.app_ecr_repo.repository_url
+}
