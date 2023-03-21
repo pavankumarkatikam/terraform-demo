f56e5f14b6d93e601e7f503f92bfc3f6ff0ebbac
pavankumarkatikam <42901841+pavankumarkatikam@users.noreply.github.com>
2023-03-21 08:24:57 +0530

    Update repository.tf

--- ecs-repo/repository.tf
+++ ecs-repo/repository.tf
@@ -1,3 +1,15 @@
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "terr-remote-backend"
+    key    = "repo/repobackend.tfstate"
+    region = "us-east-2"
+  }
+}
+
+
 resource "aws_ecr_repository" "repo" {
   name                 = "app_repo"
   image_tag_mutability = "MUTABLE"
